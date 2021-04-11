5669a6320faa459cdc3f24b32a165762df1eb0c7
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2021-04-11 11:13:48 +0800

    feat: img

--- pages/components/Swiper/data.js
+++ pages/components/Swiper/data.js
@@ -1,18 +1,22 @@
 export const slideList = [
   {
-    img: 'https://www.bayer.com.cn/Public/new_v/img/banner/Header.jpg?1',
+    img:
+      'https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/8375782150/5861/f221/fc3e/dfc6168137781199746ce0cad53e4413.jpeg',
     desc: '',
   },
   {
-    img: 'https://www.bayer.com.cn/Public/new_v/img/banner/Header.jpg?2',
+    img:
+      'https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/8375779565/d856/b453/45f3/fdfe93e3834a902a3b32b599fc8f3ca9.jpeg',
     desc: '',
   },
   {
-    img: 'https://www.bayer.com.cn/Public/new_v/img/banner/Header.jpg?3',
+    img:
+      'https://p6.music.126.net/obj/wo3DlcOGw6DClTvDisK1/8375780544/9839/7779/a8ce/73707b195352a894979b64fe39ecff0a.jpeg',
     desc: '',
   },
   {
-    img: 'https://www.bayer.com.cn/Public/new_v/img/banner/Header.jpg?4',
+    img:
+      'https://p5.music.126.net/obj/wo3DlcOGw6DClTvDisK1/8375781464/eb3d/2d70/e985/79598e37c09f80cb64cf44c02a417d8e.jpeg',
     desc: '',
   },
 ];
