d57a865cd81d2083dca09d61cb7a035da5a44394
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2021-04-12 22:39:24 +0800

    feat: 443

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 // server.js
 const { createServer } = require('http');
+const https = require('https');
 const { parse } = require('url');
 const fs = require('fs');
 const next = require('next');
+const path = require('path');
 
 // const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-    if (pathname === '/.well-known/pki-validation/fileauth.txt') {
-      fs.readFile(__dirname + '/fileauth.txt', (err, contents) => {
-        res.setHeader('Content-Type', 'text/plain');
-        res.writeHead(200);
-        res.end(contents);
-      });
-    } else if (pathname === '/a') {
-      app.render(req, res, '/a', query);
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(80, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(
+          path.join(__dirname, './cert/5473165_www.qiushipharm.com.key'),
+          'utf8'
+        ),
+        cert: fs.readFileSync(
+          path.join(__dirname, './cert/5473165_www.qiushipharm.com.pem'),
+          'utf8'
+        ),
+      },
+      (req, res) => {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+        if (pathname === '/.well-known/pki-validation/fileauth.txt') {
+          fs.readFile(__dirname + '/fileauth.txt', (err, contents) => {
+            res.setHeader('Content-Type', 'text/plain');
+            res.writeHead(200);
+            res.end(contents);
+          });
+        } else if (pathname === '/a') {
+          app.render(req, res, '/a', query);
+        } else if (pathname === '/b') {
+          app.render(req, res, '/b', query);
+        } else {
+          handle(req, res, parsedUrl);
+        }
+      }
+    )
+    .listen(443, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
 });
