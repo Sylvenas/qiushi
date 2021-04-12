02a5f1ff21b71f21d1640b6422304f2c03fa6938
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2021-04-12 20:24:06 +0800

    feat: https

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // server.js
 const { createServer } = require('http');
 const { parse } = require('url');
+const fs = require('fs');
 const next = require('next');
 
 // const dev = process.env.NODE_ENV !== 'production';
@@ -13,8 +14,13 @@ app.prepare().then(() => {
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
-
-    if (pathname === '/a') {
+    if (pathname === '/.well-known/pki-validation/fileauth.txt') {
+      fs.readFile(__dirname + '/fileauth.txt', (err, contents) => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.writeHead(200);
+        res.end(contents);
+      });
+    } else if (pathname === '/a') {
       app.render(req, res, '/a', query);
     } else if (pathname === '/b') {
       app.render(req, res, '/b', query);
