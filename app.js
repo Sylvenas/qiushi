f2bc84030dc5a42feaab1ab2a36faf960cfb29a0
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2021-04-27 19:34:43 +0800

    feat: 80

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.prepare().then(() => {
         handle(req, res, parsedUrl);
       }
     })
-    .listen(1000, (err) => {
+    .listen(80, (err) => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
